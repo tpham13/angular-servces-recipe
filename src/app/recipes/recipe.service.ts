3482ee076349a426b4ce600f8c9755c74148b459
tpham13 <tpham0123@gmail.com>
2021-09-28 10:35:41 -0700

    Added recipe and shopping-list service

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,16 @@
+// import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
